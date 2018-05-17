ae29a0a8b74b9fe487e44b5babfbf75d463bf5d0
kevinkorte <kevinkorte15@gmail.com>
2018-05-16 21:01:07 -0700

    added bootstrap

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
  
 import App from '../../ui/layouts/App/App';
+
  
 Meteor.startup(() => {
   render(<App />, document.getElementById('react-root'));
--- imports/ui/layouts/App/App.js
+++ imports/ui/layouts/App/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import { Button } from 'reactstrap';
+import bootstrap from 'bootstrap';
 
 class App extends React.Component {
   render() {
