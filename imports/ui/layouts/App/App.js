05359fd3b8371dbe612bfb3930c736d234b4ccbc
Kevin Korte <kevinkorte15@gmail.com>
2018-05-16 17:07:50 -0700

    still playing with react

--- imports/ui/layouts/App/App.js
+++ imports/ui/layouts/App/App.js
@@ -1,13 +1,37 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 class App extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route exact name="index" path="/" component={Index} />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route exact name="index" path="/" component={Index} />
+          <Route name="signup" path="/signup" component={Signup} />
+          
+        </Switch> 
+      </Router>
+    )
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Index</div>
+        <Button color="danger">Danger!</Button>
+      </div>
+    )
+  }
+}
+
+class Signup extends React.Component {
+  render() {
+    return (
+      <div>Signup</div>
     )
   }
 }
